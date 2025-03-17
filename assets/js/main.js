f2bf7eb67f18e29200176f15cf455c249f25a36a
nishan das <nishandas880@gmail.com>
2025-03-17 15:50:48 +0600

    home page is rady

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,50 @@
 (function ($) {
     $(document).ready(function () {
 
+		// header sticky
+		var windowOn = $(window);
+		windowOn.on('scroll', function () {
+			var scroll = windowOn.scrollTop();
+				if (scroll > 50) {
+					$(".header-area").addClass("header-sticky");
+				} else {
+					$(".header-area").removeClass("header-sticky");
+				}
+		});
+
+    	// offcanvas humbarger
+		let offcanvasElement = $('.header-offcanvas');
+		offcanvasElement.on('show.bs.offcanvas', function () {
+			$('.humbarger-btn').addClass('open');
+			$('.btn-close span:nth-child(1)').css({
+				transform: 'rotate(45deg)',
+				marginBottom: '0'
+			});
+			$('.btn-close span:nth-child(2)').css({
+				transform: 'rotate(-45deg)',
+				marginTop: '-4px'
+			});
+		});
+		offcanvasElement.on('hide.bs.offcanvas', function () {
+			$('.humbarger-btn').removeClass('open');
+			$('.btn-close span:nth-child(1)').css({
+				transform: '',
+				marginBottom: ''
+			});
+			$('.btn-close span:nth-child(2)').css({
+				transform: '',
+				marginTop: ''
+			});
+		});
+    		// offcanvas menu 
+		$(".mobile-nav > ul > li > a").click(function (e) {
+        e.preventDefault();
+        let subMenu = $(this).next(".sub-menu");
+        $(".mobile-nav .sub-menu").not(subMenu).slideUp();
+        $(".mobile-nav > ul > li > a").not(this).removeClass('rotate active');
+        $(this).toggleClass('rotate active');
+        subMenu.stop(true, true).slideToggle();
+      });
 
         // nice select
         // $('select').niceSelect();
@@ -46,57 +90,104 @@
 
  
         // animation
-        gsap.registerPlugin(SplitText, ScrollTrigger);
-        let textWrappers = $(".animation-text");
-        // Split text into lines and letters
-        let mainTitleSplit = new SplitText(textWrappers, {
-            type: "lines,chars",
-            linesClass: "line-wrapper overflow-hidden",
-            charsClass: "letter",
-            tag: "span"
-        });
-        // Animate each line's letters
-        $(".line-wrapper").each(function () {
-            let letters = $(this).find(".letter");
-            gsap.from(letters, {
-                scrollTrigger: {
-                    trigger: this,
-                    start: "top bottom",
-                    end: "bottom top",
-                    toggleActions: "play none none reverse",
-                },
-                y: 50,
-                opacity: 0,
-                duration: 0.5,
-                stagger: 0.04,
-                ease: "power3.inOut"
-            });
-        });
+        gsap.registerPlugin(ScrollTrigger);
+        if (window.innerWidth > 991) {
         // animation line
         gsap.utils.toArray(".animation-line").forEach((element) => {
-            gsap.fromTo(
-                element,
-                {
-                    y: 30,
-                    opacity: 0,
-                },
-                {
-                    y: 0,
-                    opacity: 1,
-                    duration: 1.5,
-                    ease: "power2.out",
-                    scrollTrigger: {
-                        trigger: element,
-                        start: "top 90%",
-                        toggleActions: "play none none none"
+          gsap.fromTo(
+              element,
+              {
+                  y: 30,
+                  opacity: 0,
+              },
+              {
+                  y: 0,
+                  opacity: 1,
+                  duration: 1.5,
+                  ease: "power2.out",
+                  scrollTrigger: {
+                      trigger: element,
+                      start: "top 90%",
+                      toggleActions: "play none none none"
 
-                    },
-                }
-            );
+                  },
+              }
+          );
+      });
+      gsap.fromTo('.quality-img-wrapper', 
+        { x: 0 },
+        {
+          x: 50,
+          duration: 1.5,
+          ease: "power4.out",
+          scrollTrigger: {
+            trigger: '.title-line-animation-div',
+            start: 'top 50%',
+            end: 'top 30%',
+          }
+        }
+      );
+      
+      gsap.fromTo('.main-center-img', 
+        { x: 0 },
+        {
+          opacity: 1,
+          x: '60px',
+          duration: 1.5,
+          ease: "power4.out",
+          delay: 0.1,
+          scrollTrigger: {
+            trigger: '.title-line-animation-div',
+            start: 'top 50%',
+            end: 'top 30%',
+          }
+        }
+      );
+      $(".arrow-1 , .arrow-2").each(function() {
+        gsap.to($(this), {
+          x: 0,
+          y: 0,
+          duration: 2,
+          ease: "power4.out",
+          scrollTrigger: {
+            trigger: ".animation-line-wrapper",
+            start: "top 80%",
+            toggleActions: "play none none none"
+          }
         });
-        // animation
+      });
+      $(".inner-text").each(function() {
+        gsap.fromTo(this, 
+          { y: 30, opacity: 0 }, 
+          { y: 0, opacity: 1, duration: 1.5, ease: "power4.out",
+            scrollTrigger: {
+              trigger: ".animation-line-wrapper",
+              start: "top 80%",
+              toggleActions: "play none none none"
+            }
+          }
+        );
+      });
+      $('.animation-full-line').each(function() {
+        var element = $(this);
+        
+        gsap.fromTo(element[0], 
+          { width: '5%' },
+          {
+            width: '100%',
+            duration: 1.5,
+            ease: "power4.out",
+            scrollTrigger: {
+              trigger: element[0],
+              start: 'top 80%',
+              end: 'bottom top', // Adjust to a better scroll trigger endpoint
+              // markers: true,
+            }
+          }
+        );
+      });
 
-  
+        }
 
 
  
@@ -146,84 +237,9 @@
         gsap.ticker.lagSmoothing(0);
         // lenis
 
-        // animation-line-wrapper
-        gsap.to(".arrow-1", {
-            x: 0,
-            y: 0,
-            duration: 2,
-            ease: "power4.out",
-            scrollTrigger: {
-              trigger: ".animation-line-wrapper",
-              start: "top 80%",
-              toggleActions: "play none none none"
-            }
-          });
-          
-          gsap.to(".arrow-2", {
-            x: 0,
-            y: 0,
-            duration: 2,
-            ease: "power4.out",
-            scrollTrigger: {
-              trigger: ".animation-line-wrapper",
-              start: "top 80%",
-              toggleActions: "play none none none"
-            }
-          });
 
-          gsap.fromTo(".inner-text", 
-            { y: 30, opacity: 0 }, // Starting position
-            { y: 0, opacity: 1, duration: 2, ease: "power4.out",
-            //   delay: 0.3,
-              scrollTrigger: {
-                trigger: ".animation-line-wrapper",
-                start: "top 80%",
-                toggleActions: "play none none none"
-              }
-            }
-          );
-          gsap.fromTo('.animation-full-line', 
-            { width: '5%' },
-            {
-              width: '100%',
-              duration: 1.5,
-              ease: "power4.out",
-              scrollTrigger: {
-                trigger: '.title-line-animation-div',
-                start: 'top 50%',
-                end: 'top 30%',
-              }
-            }
-          );
-          
-          gsap.fromTo('.quality-img-wrapper', 
-            { x: 0 },
-            {
-              x: 50,
-              duration: 1.5,
-              ease: "power4.out",
-              scrollTrigger: {
-                trigger: '.title-line-animation-div',
-                start: 'top 50%',
-                end: 'top 30%',
-              }
-            }
-          );
-          
-          gsap.fromTo('.main-center-img', 
-            { x: 0 },
-            {
-              opacity: 1,
-              x: '60px',
-              duration: 1.5,
-              ease: "power4.out",
-              delay: 0.1,
-              scrollTrigger: {
-                trigger: '.title-line-animation-div',
-                start: 'top 50%',
-                end: 'top 30%',
-              }
-            }
-          );
+
+
+        
     });
 })(jQuery);
